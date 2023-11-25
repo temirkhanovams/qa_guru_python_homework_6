657a0d14730993a175b8d4c3ab7a3a35f83de2f9
temirkhanovams <marinchik.t.s@gmail.com>
2023-11-25 13:46:51 +0300

    Добавление дополнительное условие когда пользователь сам включил тёмную тему

--- homework.py
+++ homework.py
@@ -28,13 +28,29 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    if dark_theme_enabled_by_user == None:
+    # if dark_theme_enabled_by_user is True:
+    #     is_dark_theme = True
+    # else:
+    #     if dark_theme_enabled_by_user is None:
+    #         if 6 < current_time.hour <= 22:
+    #             is_dark_theme = False
+    #         else:
+    #             is_dark_theme = True
+    #     else:
+    #         is_dark_theme = False
+    #
+    # assert is_dark_theme is True
+
+    if dark_theme_enabled_by_user is None:
         if 6 < current_time.hour <= 22:
             is_dark_theme = False
         else:
             is_dark_theme = True
     else:
-        is_dark_theme = True
+        if dark_theme_enabled_by_user is True:
+            is_dark_theme = True
+        else:
+            is_dark_theme = False
 
     assert is_dark_theme is True
 
@@ -90,10 +106,9 @@ def make_readable_function(func, *args):
 
 
 def test_readable_function():
-
-     open_browser(browser_name="Chrome")
-     go_to_companyname_homepage(page_url="https://companyname.com")
-     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
+    open_browser(browser_name="Chrome")
+    go_to_companyname_homepage(page_url="https://companyname.com")
+    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
 def open_browser(browser_name):
